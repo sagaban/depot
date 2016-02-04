c82a2f75cd22ee740bad27f2b3b5f8ad4be516a1
Santiago Bandiera <santiago@mbp13.local>
2016-02-04 20:13:37 +0100

    Change product fixture. Add new tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products #This is loaded by default
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -44,6 +46,29 @@ class ProductTest < ActiveSupport::TestCase
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
+  end
+
+  test "product is not valid without a unique title" do
+    # puts products(:ruby).description
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert product.invalid?
+    # puts product.errors[:title]
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+
+  #To avoid using hardcoded error messages
+  test "product is not valid without a unique title - i18n" do
+
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
 
 end
