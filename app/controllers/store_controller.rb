1658b151d6956396f9e4f78cff5e574761177e9e
Santiago Bandiera <santiago@mbp13.local>
2016-02-06 20:24:01 +0100

    Add the Store controller. Change its view and styles. Add it as root in routes

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,9 @@
+class StoreController < ApplicationController
+=begin
+  edit the file index.html.erb in app/views/store. (Remember that the path name to the view is built from the name of
+  the controller [store] and the name of the action [index]
+=end
+  def index
+    @products = Product.order(:title)
+  end
+end
