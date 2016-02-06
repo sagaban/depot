32d77afb19710066af9fd99d3f4c3f1239ee92d1
Santiago Bandiera <santiago@mbp13.local>
2016-02-06 21:30:01 +0100

    Add price format. Add functional test for store

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,14 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
+
+=begin
+  Both validation and functional tests will test the behavior of controllers only; they will not retroactively affect any objects that already exist in the database or in fixtures. In the previous example, two products contain the same title. Such data will cause no prob- lems and will go undetected up to the point where such records are modified and saved.
+=end
