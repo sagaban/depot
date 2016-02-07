918cab189e6fa6a09d3f7fd08e1524e9b4815396
Santiago Bandiera <santiago@mbp13.local>
2016-02-07 16:06:04 +0100

    Migrate DB. Create current cart concern

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203194452) do
+ActiveRecord::Schema.define(version: 20160207145136) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
