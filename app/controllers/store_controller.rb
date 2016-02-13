c5838939ac65bbd4a70910e91355ca22e34bd3d9
Santiago Bandiera <santiago@hanzo.es>
2016-02-13 15:34:39 +0100

    Add a partial of cart in the store page. Updat css, flow and old cart page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,6 +3,8 @@ class StoreController < ApplicationController
   edit the file index.html.erb in app/views/store. (Remember that the path name to the view is built from the name of
   the controller [store] and the name of the action [index]
 =end
+  include CurrentCart
+  before_action :set_cart
   def index
     @products = Product.order(:title)
   end
